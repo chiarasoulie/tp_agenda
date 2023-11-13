8745e8b08802551dd3e5d59989dc3a87ab77c434
souliechiara@gmail.com <souliechiara@gmail.com>
2023-11-13 11:05:54 +0100

    changement

--- src/main/java/agenda/FixedTerminationEvent.java
+++ src/main/java/agenda/FixedTerminationEvent.java
@@ -1,19 +1,18 @@
 package agenda;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDate;
 import java.time.temporal.ChronoUnit;
+
 /**
  * Description : A repetitive event that terminates after a given date, or after
  * a given number of occurrences
  */
 public class FixedTerminationEvent extends RepetitiveEvent {
 
-    private LocalDate terminationInclusive;
-    private long numberOfOccurrences;
-    private LocalDate calculTermination;
 
-    
     /**
      * Constructs a fixed terminationInclusive event ending at a given date
      *
@@ -28,49 +27,32 @@ public class FixedTerminationEvent extends RepetitiveEvent {
      * </UL>
      * @param terminationInclusive the date when this event ends
      */
+    private LocalDate terminationInclusive;
+    private long numberOfOccurrences;
+
     public FixedTerminationEvent(String title, LocalDateTime start, Duration duration, ChronoUnit frequency, LocalDate terminationInclusive) {
-         super(title, start, duration, frequency);
-        long nbreOccu = frequency.between(start.toLocalDate(), terminationInclusive);
-        this.numberOfOccurrences = nbreOccu;
+        super(title, start, duration, frequency);
+        this.terminationInclusive = terminationInclusive;
 
     }
 
-    /**
-     * Constructs a fixed termination event ending after a number of iterations
-     *
-     * @param title the title of this event
-     * @param start the start time of this event
-     * @param duration the duration of this event
-     * @param frequency one of :
-     * <UL>
-     * <LI>ChronoUnit.DAYS for daily repetitions</LI>
-     * <LI>ChronoUnit.WEEKS for weekly repetitions</LI>
-     * <LI>ChronoUnit.MONTHS for monthly repetitions</LI>
-     * </UL>
-     * @param numberOfOccurrences the number of occurrences of this repetitive event
-     */
+
     public FixedTerminationEvent(String title, LocalDateTime start, Duration duration, ChronoUnit frequency, long numberOfOccurrences) {
         super(title, start, duration, frequency);
         this.numberOfOccurrences = numberOfOccurrences;
-        calculTermination = start.toLocalDate().plus(numberOfOccurrences, frequency);
     }
 
     /**
-     *
      * @return the termination date of this repetitive event
      */
     public LocalDate getTerminationDate() {
-        return calculTermination;
+        return getStart().toLocalDate().plus(this.numberOfOccurrences - 1, getFrequency());
+
     }
 
     public long getNumberOfOccurrences() {
-        return numberOfOccurrences;
+
+        return this.getFrequency().between(this.getStart().toLocalDate(), terminationInclusive.plus(1, this.getFrequency()));
     }
-    public String toString() {
-        return super.toString() +
-                "terminationInclusive=" + terminationInclusive +
-                ", numberOfOccurrences=" + numberOfOccurrences +
-                '}';
-    }
-        
+
 }
